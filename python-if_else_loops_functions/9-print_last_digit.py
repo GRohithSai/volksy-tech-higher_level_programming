69578212216361d0b33fb3baae926600d0fd1167
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-02 04:19:57 -0800

    M

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = number % 10
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
-
+    a = abs(number) % 10
+    print(a, end="")
+    return a
