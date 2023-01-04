a831986856741ae503a382788a8ac255d05b053b
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-04 02:10:03 -0800

    M

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
         print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     operator = argv[2]
     n1 = int(argv[1])
     n2 = int(argv[3])
