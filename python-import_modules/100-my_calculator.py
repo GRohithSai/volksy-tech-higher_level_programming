a93d4d48bb317d7ff511d67fcd5856be2a6d3198
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-04 02:02:12 -0800

    M

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("./100-my_calculator.py <a> <operator> <b>")
     operator = argv[2]
-    n1 = int(arv[1])
+    n1 = int(argv[1])
     n2 = int(argv[3])
     if operator == "+":
         print("{} + {} = {}".format(n1, n2, add(n1, n2)))
