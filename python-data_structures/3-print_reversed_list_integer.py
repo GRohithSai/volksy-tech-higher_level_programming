dbd43807edbbab0a6ef465a51a135e3985a92e56
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-05 01:52:32 -0800

    M

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list = []):
     for i in my_list:
         if i = len(my_list)-1:
-            print(i)
-            i = i -1
+            print("{:d}".format(i))
+            len(my_list) -= 1
