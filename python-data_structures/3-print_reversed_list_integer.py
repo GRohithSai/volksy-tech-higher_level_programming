b223639f1478683aca3c910e2eb004d24bda2093
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-05 03:44:01 -0800

    M

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(digits) // 2):
         digits[i], digits[-1 - i] = digits[-1 -i], digits[i]
-    print("{:d"}".format(i))
+        print("{:d"}".format(i))
