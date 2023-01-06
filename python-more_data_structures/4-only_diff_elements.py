e83cc12a380f9d367bf57ca8eb04590b33a54a19
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-06 02:50:52 -0800

    M

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if set_1 = {} and set_2 ={}:
+    if set_1 = {} and set_2 = {}:
         set_3 = set_1.symmetric_difference_update(set_2)
-        print(list(set_2))
+        print(list(set_3))
