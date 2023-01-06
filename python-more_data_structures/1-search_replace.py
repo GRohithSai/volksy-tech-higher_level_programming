a9ab685c00986d6ac709e1668035274b4c74ec07
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-06 02:11:03 -0800

    M

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def search_raplce(my_list, search, replace):
-    new_list = my_list.replace(search, replace)
+    new_list = my_list.search(2)
+    new_list.replace(2, 89)
+    print(new_list)
+    print(my_list)
