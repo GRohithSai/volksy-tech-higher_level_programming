ca7945a8285e0b01942dff7d0eb81f3df5bff896
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-09 02:42:17 -0800

    M

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash/python3
+def safe_print_list_integers(my_list=[], x=0):
+e = 0
+for i in range(x):
+    try:
+        print("{:d}".format(my_list[i]), end="")
+        e += 1
+    except (ValueError, TypeError):
+        continue
+    except (IndexError):
+        raise
+print()
+return e
+
