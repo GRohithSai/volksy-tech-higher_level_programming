e9179c1470245636b27571d48c6c0d7aee2c9994
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-09 02:56:35 -0800

    M

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,8 +4,8 @@ def safe_print_division(a, b):
     b = 2
     result = safe_print_division(a, b)
     try:
-        print("{:d} / {:d} = {}". format(a, b, result))
+        print("{:d} / {:d} = {}".format(a, b, result))
     except:
         pass
     finally:
-        print("{:d} / {:d} = {}". format(a, b, result))
+        print("{:d} / {:d} = {}".format(a, b, result))
