43107ef230c408290affe91614f907c1ae84aea5
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-11 03:19:31 -0800

    M

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        return (fct(*args))
+    except Exception a err:
+        print("Excption: {}".fomrat(err), file=stderr)
+        return None
