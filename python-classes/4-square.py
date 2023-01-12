ee4ec57e063093b4a6d7a8e6dcaaf2157d943608
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-11 20:52:43 -0800

    M

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pythom3
+"""creating class"""
+
+
+class Square:
+    """Square with private instance attribute and instantiation"""
+    def __init__(self, size=0):
+        """initailize data"""
+        self.__size=size
+
+    def area(self):
+        """return square area"""
+        return(self.__size ** 2)
+        
+    @property
+    def size(self):
+        """retrive size"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """handling errors"""
+        if type(value) is not int:
+            raise TYpeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
