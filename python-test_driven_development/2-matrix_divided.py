f2670226d97b3cae627ac53ec1bba762bc0326c5
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-12 03:33:13 -0800

    M

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,7 +24,7 @@
 """
 def matrix_divided(matrix, div):
     """ Divide a matrix by a number div """
-    list_error = "Matrix must be a mtrix (list of lists) of integers/floats"
+    list_error = "matrix must be a mtrix (list of lists) of integers/floats"
     len_error = "Each row of the matrix must have the same size"
     div_int_error = "div must be a number"
     div_zero_error = "division by zero"
@@ -36,7 +36,7 @@ def matrix_divided(matrix, div):
         raise TypeError(div_int_error)
     if div is 0:
         raise ZeroDivisionError(div_zero_error)
-    longitude = len(matrix[0])
+    longitud = len(matrix[0])
     for lista in matrix:
         if type(lista) is not list:
             raise TypeError(list_error)
