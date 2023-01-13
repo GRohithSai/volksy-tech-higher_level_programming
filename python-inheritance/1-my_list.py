8ec394a247f0e11a3df89d0a656c08afa51e170c
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-12 23:22:04 -0800

    M

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Mylist that inherits from list"""
+
+
+Class Mylist(List):
+    """Mylist inherits from list"""
+
+    def print_sorted(self):
+        """"Prints list in ascending order"""
+        print(sorted(self))
+
