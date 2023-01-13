80613028a493ff1a37abed346498157ec568acd9
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-12 23:13:13 -0800

    M

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Returns the list of availabla attributes and methods""""
 
+
 def lookup(obj):
     """Search for all the attributes and methods of object"""
     return (method for method in dir(obj))
