3fd25de3dcfd4be3def9bc23cea38b6ae95a858a
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-18 04:18:44 -0800

    M

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list, then saves them to a file"""
+"""Script that adds all arguments to a
+Python list,
+then saves them to a file"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -10,7 +12,7 @@ try:
     List = load_from_json_file(fname)
 except:
     pass
-    
+
 for i in range(1, len(sys.argv)):
     List.append(sys.argv[i])
 save_to_json_file(List, fname)
