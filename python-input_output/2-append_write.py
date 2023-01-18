387e7d0a00b9ccd44c88d8c1191ae5d4915d8543
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-18 02:20:42 -0800

    M

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -18,13 +18,13 @@ def read_lines(filename="", nb_lines=0):
         lines = 0
         for line in f:
             lines += 1
-            f.seek(0)
-            if nb_lines >= lines:
-                print(f.read(), end="")
-                return
+        f.seek(0)
+        if nb_lines >= lines:
+            print(f.read(), end="")
+            return
 
-            else:
-                n = 0
-                while n < nb_lines:
-                    print(f.readline(), end="")
-                    n += 1
+        else:
+            n = 0
+            while n < nb_lines:
+                print(f.readline(), end="")
+                n += 1
