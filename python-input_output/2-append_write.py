a8f3bccf5818a4a325dc6102daf6174d092d3a48
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-18 02:29:02 -0800

    M

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 """Defines a text file-reading function."""
 
 
-def read_lines(filename="", nb_lines=0):
+def append_write(filename="", text=""):
     """Print a given number of lines from a UTF8 text file to stdout."""
     if filename:
         with open(filename, mode='a', encoding="utf-8") as f:
