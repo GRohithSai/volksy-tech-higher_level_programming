2810c64e4fd04babe9e3ab9dcd26d28308f7ff89
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-18 05:02:00 -0800

    M

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -11,6 +11,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student."""
 
