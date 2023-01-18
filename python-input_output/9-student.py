9bed56a6ec48932dae023dd6e3b783bc79186487
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-18 04:33:59 -0800

    M

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,7 +6,7 @@ class Student:
     """Representa student."""
 
     def __init__(self, first_name, last_name, age):
-        """Iniatialize a new Student.
+        """Iniatialize a new Student."""
 
     def to_json(self):
         """Get a dictionary representation of the student."""
