9dcb213ea7c99a22b74317483ef9feb5baa324d5
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-29 22:09:58 -0800

    M

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- It is used to count the number of records with the same value
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY(score) ORDER BY DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY(score) ORDER BY(number) DESC;
