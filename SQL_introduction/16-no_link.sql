6e185d96ff688fbbd69c30e24d9526d7355cebdf
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-29 22:17:25 -0800

    M

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- It is used to display all the record in a table in order
-SELECT score, name FROM second_table ORDER BY(score) DESC ORDER BY(name);
+SELECT score, name FROM second_table WHERE name != "" ORDER BY(score) DESC;
