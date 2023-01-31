bdc43432125a209b5849aca14ade222e73dbc265
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 05:02:43 -0800

    M

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,4 @@
 -- It is used to display the values in a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_od_usa;
-CREATE TABLE IF NOT EXISTS states(id INT, name VARCHAR(256));
-INSERT INTO states VALUES(1, "California"), (2, "Arizona"), (3, "Texas"), (4, "Utah");
-CREATE TABLE IF NOT EXISTS cities(id INT, state_id INT, name VARCHAR(256));
-INSERT INTO cities VALUES(1, 1, "San Fransisco"), (2, 1, "San Jose"), (4, 2, "Page"), (6, 3, "Paris"), (7, 3, "Houston"), (8, 3, "Dallas"));
 SELECT id, name FROM cities WHERE state_id IN(SELECT id FROM states WHERE name = "California") ORDER BY id;
