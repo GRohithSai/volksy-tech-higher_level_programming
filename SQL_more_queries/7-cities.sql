ecd2f7dcad4db416483a6a4e5ae0536b9a3b371d
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 04:06:51 -0800

    M

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- It is used to create a database and the table in the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS(id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)), name VARCHAR(256));
