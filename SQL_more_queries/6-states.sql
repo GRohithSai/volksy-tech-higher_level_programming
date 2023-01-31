71db489ab1f6ad39d2cc51336feaa0ec613bb58a
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 03:59:49 -0800

    M

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- It is used to create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
