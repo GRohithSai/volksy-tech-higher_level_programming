630f2aa204d0cc309285a7070c89db96c6c9b296
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 04:46:19 -0800

    M

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS states(id INT, name VARCHAR(256));
 INSERT INTO states VALUES(1, "California"), (2, "Arizona"), (3, "Texas"), (4, "Utah");
 CREATE TABLE IF NOT EXISTS cities(id INT, state_id INT, name VARCHAR(256));
 INSERT INTO cities values(1, 1, "San Fransico"), (2, 1, "San Jose"), (4, 2, "Page"), (6, 3, "Paris"), (7, 3, "Houston"), (8, 3, "Dallas"));
-SELECT id, name FROM cities UNION SELECT id, name FROM states where name = "California";
+SELECT id, name FROM cities UNION SELECT id, name FROM states WHERE name = "California";
