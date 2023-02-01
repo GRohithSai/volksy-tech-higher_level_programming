cf7f1bdfe32d5d3fc7e88fc73963e7aa4a05ee25
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 23:29:39 -0800

    M

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- It is used to creata the user with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'='user_0d_1_pwd'; 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
