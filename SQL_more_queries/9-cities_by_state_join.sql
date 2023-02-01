e261e435d224d9b1648dedeff41e18d9f438aa58
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 20:39:15 -0800

    M

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- It is used to display the records in a table with condition
-SELECT c.id, c.name, s.name FROM cities as c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id;
+SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id;
