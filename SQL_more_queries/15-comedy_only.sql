bd1df379323c25d728978f1d89720c02751bde80
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 22:49:51 -0800

    M

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- It is used to display all comedy shows in the database
-SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_d INNER JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_d INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
