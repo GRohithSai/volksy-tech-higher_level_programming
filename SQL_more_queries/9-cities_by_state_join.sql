bec77aec0d3ed303c75f2d8076b06f95157e473d
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 20:33:40 -0800

    M

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- It is used to display the records in a table with condition
-SELECT id, name FROM cities INNER JOIN states;
+SELECT id, name FROM cities INNER JOIN states ON cities.state_id = states.id;
