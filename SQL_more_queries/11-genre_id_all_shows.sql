ced9684cfe4aec69d32a9f54b8de24a03d0f16ca
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 20:59:09 -0800

    M

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- It is used to display records in a database
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_shows_genre ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
