d823a167b5e673afa83afb1440a8f4c0575872e9
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 21:18:41 -0800

    M

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- It is used to display data in a database
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE genre_id is NULL ORDER BY tv_shows, tv_show_genres;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE genre_id is NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
