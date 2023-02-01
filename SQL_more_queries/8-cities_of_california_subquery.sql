d3544da54d28a3610a7be48a1631da45c910f9ed
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 20:26:47 -0800

    M

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,2 @@
 -- It is used to display the values in a table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id IN(SELECT id FROM hbtn_0d_usa.states WHERE name = "California") ORDER BY id;
+SELECT id, name FROM cities WHERE state_id IN(SELECT id FROM states WHERE name = "California") ORDER BY id;
