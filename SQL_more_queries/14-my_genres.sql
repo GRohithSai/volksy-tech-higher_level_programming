b31e03edb75450c4ab8f5cf1e263ddcfcda87d98
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 22:25:11 -0800

    M

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- It is used to list all genres of the show Dexter
-SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name;
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name;
