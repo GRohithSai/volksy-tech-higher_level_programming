8c8018dbba747fb94e3f60980bda894d06bf004a
GRohithSai <rohithsai@brightcomgroup.com>
2023-01-31 21:31:35 -0800

    M

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- It is used to display values in a database
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
