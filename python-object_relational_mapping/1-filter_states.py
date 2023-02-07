bb6c8ea6c130c37ca879b198bc4013d515ed307c
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-06 22:14:09 -0800

    M

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,13 @@
 """Lists all states with a name starting with N"""
 
 
-
 import sys
 import MySQLdb
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id")
+    c.execute("SELECT * FROM states ORDER BY id WHERE name REGEXP '^[N]' ")
     s = c.fetchall()
     for i in s:
-        if i[1][0] == "N"
             print(i)
