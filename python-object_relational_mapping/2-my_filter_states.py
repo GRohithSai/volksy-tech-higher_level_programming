c7a72a9c16386cb321af3b06fdddacef3a19f205
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-14 21:26:28 -0800

    M

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """It is used to filter states by user input"""
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
 
-    if len(sys.argv) != 4:
-        sys.exit('Use: -filter_states.py <mysql username> <mysql password>' 'database name>')
-        conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset='utf-8')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' " "ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
+    for i in c:
+        print(i)
