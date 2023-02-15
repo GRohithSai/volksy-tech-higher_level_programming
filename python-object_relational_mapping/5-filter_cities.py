8d9575a3aeaf3ce29f6559c4e50216b27f548bf8
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-14 21:48:11 -0800

    M

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""python script that lists all cities from database hbtn_0e_4_usa"""
-
-
+"""
+python script that lists all cities from the database hbtn_0e_4_usa
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
-                         passwd = arv[2], db=argv[3], charset="utf-8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT cities.name FROM cities \
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
