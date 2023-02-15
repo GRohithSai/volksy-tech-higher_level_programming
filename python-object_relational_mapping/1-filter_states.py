9d1b247f35586604de2f02c8b8cc769e863d688d
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-14 21:14:33 -0800

    M

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""It displays all the records with given condtion"""
+"""WHERE statement"""
 
 
 import sys
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id")
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
     for i in c:
         if i[1][0] == "N":
             print(i)
