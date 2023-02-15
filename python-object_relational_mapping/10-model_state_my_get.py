62814db05bbdb624c4baab6c50f10f714d4801f8
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-14 22:24:21 -0800

    M

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-"""Lists all State objects from database hbtn_0e_6_usa containing "a"
 """
-import sys
+Created on Sat Aug  8 09:05:11 2020
+@author: Robinson Montes
+"""
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
 
-if __name__ == "__main__":
-    """ Engine connection
-    """
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    state_name = args[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    """ Session handling
-    """
-
+                           .format(username, password, data))
+    # create custom session object class from database engine
     Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
     session = Session()
-
-    """ Query
-    """
-
-    for state in session.query(State)\
-                        .order_by(State.id)\
-                        .filter(State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))
-
-        session.close()
+    states = session.query(State).filter(State.name == state_name)\
+                    .order_by(State.id)
+    if states is not None and states.count() > 0:
+        for state in states:
+            print('{}'.format(state.id))
+    else:
+        print('Not found')
