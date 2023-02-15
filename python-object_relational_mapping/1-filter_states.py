cb8c6b503016080b2bb8978389a704bf15b61026
GRohithSai <rohithsai@brightcomgroup.com>
2023-02-14 20:56:20 -0800

    M

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N"""
 
 
-
-import sys
-import MySQLdb
-
+def getAllstates(user2, password2, db2):
+    """Script that gets all the states when called on"""
+    import MySQLdb
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user2,passwd=password2, db=db2, charset="utf-8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id")
-    data = c.fetchall()
-    for name in data:
-        if name[1][0]= "N":
-            print(name)
+    import sys
+    """ protected from executing when imported"""
+    getAllstates(sys.argv[1], sys.argv[2], sys.argv[3])
