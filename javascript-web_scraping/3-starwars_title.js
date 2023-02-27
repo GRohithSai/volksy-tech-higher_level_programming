af0c6167f3c234c3504ae6b260045ff4485925d3
Rohith Sai <rohithsai@brightcomgroup.com>
2023-02-27 10:22:19 +0000

    M

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
 const filmId = process.argv[2];
 request.get(baseUrl + filmId, (err, response, body) => {
-  if(err === null) {
+  if (err === null) {
     const data = JSON.parse(body);
     console.log(data.title);
   } else {
